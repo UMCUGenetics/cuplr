7b1cc8856e6677a2ee5da3f546c7909c828ef269
Luan Nguyen <nguyen.n.luan777@gmail.com>
2020-08-24 10:36:34 +0200

    - pvalue can now be used for univariate feature selection - Adjusted predict.randomForestEnsemble to accomodate categorical features

--- training/R/utils.R
+++ training/R/utils.R
@@ -1,5 +1,14 @@
 ################################################################################
+#' Preserve order of dataframe values
+#'
+#' @description Converts character columns into factors to preserve the order of their values
+#' @param df A dataframe
+#'
+#' @return A data frame with characters converted into factors
+#' @export
+#'
 forceDfOrder <- function(df){
+   if(!is.data.frame(df)){ stop('Input must be a dataframe') }
    as.data.frame(lapply(df, function(i){
       if(!is.numeric(i)){ i <- factor(i, unique(i)) }
       return(i)
@@ -7,9 +16,21 @@ forceDfOrder <- function(df){
 }
 
 ################################################################################
+#' Prepares a feature dataframe for multiclass classifier training
+#'
+#' @description Separates a dataframe containing features and response variable. The response
+#' variable is also one hot encoded
+#'
+#' @param df A dataframe
+#' @param colname.response Name of the response column
+#'
+#' @return A list containing the feature matrix/dataframe, response vector, and response
+#' one-hot encode matrix
+#' @export
+#'
 dfToFeaturesAndResponse <- function(df, colname.response='response'){
    x <- df[,colnames(df)!=colname.response]
-   x <- as.matrix(x)
+   #x <- as.matrix(x)
 
    y <- as.factor(df[,colname.response])
    y_ohe <- oneHotEncode(y, sample.names=rownames(x))
@@ -20,13 +41,13 @@ dfToFeaturesAndResponse <- function(df, colname.response='response'){
 ################################################################################
 univarFeatSel <- function(
    x, y,
-   max.qvalue=0.01, sel.top.n.features=NULL, return.new.x=T,
-   verbose=F
+   max.qvalue=0.01, max.pvalue=NULL, sel.top.n.features=NULL,
+   return.new.x=T, verbose=F
 ){
    # colname.response='response'
    # x <- df[,colnames(df)!=colname.response]
    # y <- as.factor(df[,colname.response])
-   # #y <- y=='Prostate'
+   # y <- y=='Prostate'
 
    if( !(is.logical(y) | is.factor(y)) ){
       stop('`y` must be a logical or factor')
@@ -37,11 +58,13 @@ univarFeatSel <- function(
       #y.logical=y
       if(is.numeric(v)){
          #v=x$viral_ins.Hepatitis_C_virus
+         #v=x$rmd.14q_107
          v_split <- split(v, y.logical)
          wilcox.test(v_split[['TRUE']], v_split[['FALSE']])$p.value
       } else {
          #v=ifelse(x$purple.gender,'male','female')
          #v=unname(m[,'AR'])
+         #v=x$gene_def.VHL
          #y=metadata[ match(rownames(m), metadata$sample),'cancer_type' ]
          #y.logical <- y=='Prostate'
 
@@ -62,18 +85,22 @@ univarFeatSel <- function(
 
    if(is.logical(y)){
       if(verbose){ counter <- 0 }
-      p_values <- apply(x, 2, function(i){
+      p_values <- unlist(lapply(as.data.frame(x), function(i){
          if(verbose){
             counter <<- counter + 1
             message('[',counter,'] ', colnames(x)[[counter]] )
          }
          main(i, y)
-      })
+      }))
       p_values <- sort(p_values)
-
       q_values <- p.adjust(p_values, method='bonferroni')
 
-      keep_features <- names(q_values)[ q_values < max.qvalue ]
+      if(!is.null(max.pvalue)){
+         keep_features <- names(p_values)[ p_values < max.qvalue ]
+      } else {
+         keep_features <- names(q_values)[ q_values < max.qvalue ]
+      }
+
       if(!is.null(sel.top.n.features)){ keep_features <- keep_features[1:sel.top.n.features] }
       keep_features <- na.exclude(keep_features)
 
@@ -87,7 +114,7 @@ univarFeatSel <- function(
             counter <<- counter + 1
             message('[',counter,'] ', names(y_logicals)[[counter]] )
          }
-         apply(x, 2, function(feature){ main(feature, y_logical) })
+         unlist(lapply(as.data.frame(x), function(feature){ main(feature, y_logical) }))
       }))
       m_q_values <- apply(m_p_values, 2, p.adjust, method='bonferroni')
 
@@ -99,7 +126,7 @@ univarFeatSel <- function(
    }
 
    if(return.new.x){
-      return(x[,keep_features])
+      return(x[,keep_features,drop=F])
    }
    return(keep_features)
 }
@@ -107,128 +134,55 @@ univarFeatSel <- function(
 
 
 ################################################################################
-predict.randomForestEnsemble <- function(object, newdata, type='response'){
+#' Predict method for random forest ensemble
+#'
+#' @param object An object of class randomForestEnsemble
+#' @param newdata A data frame or matrix containing new data. (Note: If not given, the out-of-bag
+#' prediction in object is returned)
+#' @param type 'response', 'prob' or 'votes', indicating the type of output: predicted values,
+#' matrix of class probabilities, or matrix of vote counts. 'class' is allowed, but automatically
+#' converted to "response", for backward compatibility.
+#'
+#' @return 'response': predicted classes (the classes with majority vote). 'prob' matrix of class
+#' probabilities (one column for each class and one row for each input). 'vote'
+#' matrix of vote counts (one column for each class and one row for each new input); either in raw
+#' counts or in fractions (if norm.votes=TRUE).
+#' @export
+#'
+predict.randomForestEnsemble <- function(object, newdata, type='response', verbose=F){
    # object=model
    # newdata=test_data$x
    # type='response'
 
+   categorical_feature_names <- names(object[[1]]$categorical_lvls)
+   x <- as.data.frame(lapply(colnames(newdata), function(i){
+      #i='gene_def.AR'
+      if(!(i %in% categorical_feature_names)){
+         return(newdata[,i])
+      } else{
+         factor(newdata[,i], levels=object[[1]]$categorical_lvls[[i]])
+      }
+   }))
+   rownames(x) <- rownames(newdata)
+   colnames(x) <- colnames(newdata)
+
+   if(verbose){ counter <- 0 }
    m <- do.call(cbind, lapply(object, function(i){
-      randomForest:::predict.randomForest(i, newdata, type='prob')[,1]
+      #i=object$Breast
+      if(verbose){
+         counter <<- counter + 1
+         message('[RF ',counter,'/',length(object),']: ', names(object)[counter])
+      }
+      randomForest:::predict.randomForest(i, x, type='prob')[,1]
    }))
 
    if(type=='prob'){
       m
    } else {
-      factor(
-         colnames(m)[ max.col(m) ],
-         levels=colnames(m)
-      )
+      factor( colnames(m)[ max.col(m) ], levels=colnames(m) )
    }
 }
 
-getPredictions <- function(model, x, output.classes=F){
-   ## Neural network
-   if(any(grepl('keras',class(model)))){
-
-      class_names <- sapply(0:(reticulate::py_len(model$class_names)-1),function(i){
-         model$class_names[[i]]
-      })
-
-      prob <- model$predict(unname(x))
-      colnames(prob) <- class_names
-      rownames(prob) <- if(is.list(x) & !is.data.frame(x)){
-         rownames(x[[1]])
-      } else {
-         rownames(x)
-      }
-
-      if(!output.classes){ return(prob) }
-      #return( factor(class_names[apply(prob,1,which.max)], class_names) )
-      return( factor(class_names[max.col(prob)], class_names) )
-   }
-
-   ## Random forest
-   if('randomForest' %in% class(model)){
-      if(output.classes){
-         return(randomForest:::predict.randomForest(model, x, type='response'))
-      } else {
-         return(randomForest:::predict.randomForest(model, x, type='prob'))
-      }
-   }
-
-   ## Random forest ensemble
-   if('randomForestEnsemble' %in% class(model)){
-      if(output.classes){
-         return(predict.randomForestEnsemble(model, x, type='response'))
-      } else {
-         return(predict.randomForestEnsemble(model, x, type='prob'))
-      }
-   }
-
-   stop('Model must be a randomForest or Keras model')
-}
-
-################################################################################
-permutationImportance <- function(model, x, y, metric='prec', seed=NULL, verbose=T){
-   #x <- train_data$x
-   #y <- train_data$y
-
-   #if(!is.matrix(x)){ stop('`x` must be a matrix') }
-   if(!is.factor(y)){ stop('`y` must be a factor') }
-   if(length(metric)>1){ stop('Only a single metric is allowed') }
-
-   if(!is.null(seed)){ set.seed(seed) }
-
-
-   if(verbose){ message('Predicting base classes...') }
-   base_classes <- getPredictions(model, x, output.classes=T)
-
-   if(verbose){ message('Permuting features...') }
-   if(is.list(x) & !is.data.frame(x)){
-      x_as_matrix <- do.call(cbind,unname(x))
-   } else {
-      x_as_matrix <- x
-   }
-
-   x_permuted <- lapply(1:ncol(x_as_matrix),function(i){
-      #i=1
-      m <- x_as_matrix
-      m[,i] <- sample(m[,i])
-      return(m)
-   })
-
-   if(is.list(x) & !is.data.frame(x)){
-      feature_groups <- sapply(x,colnames)
-      x_permuted <- lapply(x_permuted, function(i){
-         lapply(feature_groups,function(j){ i[,j] })
-      })
-   }
-
-   if(verbose){ message('Predicting permuted classes...') }
-   perm_classes <- as.data.frame(lapply(x_permuted,function(i){
-      getPredictions(model, i, output.classes=T)
-   }), col.names=colnames(x_as_matrix), check.names=F)
-
-
-   if(verbose){ message('Calculating decrease in performance...') }
-   base_metric <- calcPerf(
-      confusionMatrix(oneHotEncode(base_classes), y, simplify=T),
-      metric
-   )[[metric]]
-
-   perf_decrease <- lapply(1:ncol(perm_classes),function(i){
-      pert_metric <- calcPerf(
-         confusionMatrix(oneHotEncode(perm_classes[,i]), y, simplify=T),
-         metric
-      )[[metric]]
-
-      base_metric - pert_metric
-   })
-   perf_decrease <- do.call(cbind, perf_decrease)
-   dimnames(perf_decrease) <- list(levels(y), colnames(x_as_matrix))
-
-   return(perf_decrease)
-}
 
 
 
