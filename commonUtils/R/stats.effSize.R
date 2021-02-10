ec040e79f298efb9d600082ddaf599e5b57ae163
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-02-10 10:09:55 +0100

    - Added function to calculate cramer's V - Added export for S3 methods

--- commonUtils/R/stats.effSize.R
+++ commonUtils/R/stats.effSize.R
@@ -1,4 +1,4 @@
-#' Calculate Cliff's delta values
+#' Calculate Cliff's delta values (for continuous data)
 #'
 #' @rdname cliffDelta
 #'
@@ -50,6 +50,7 @@ Rcpp::cppFunction('double cliffd(NumericVector x, NumericVector y){
 
 #' @rdname cliffDelta
 #' @method cliffDelta default
+#' @export
 cliffDelta.default <- function(x,y){
    #x <- m_true[,1]
    #y <- m_false[,1]
@@ -59,6 +60,7 @@ cliffDelta.default <- function(x,y){
 
 #' @rdname cliffDelta
 #' @method cliffDelta matrix
+#' @export
 cliffDelta.matrix  <- function(x, y){
    #x=m_true
    #y=m_false
@@ -88,16 +90,117 @@ cliffDelta.matrix  <- function(x, y){
 }
 
 #' @rdname cliffDelta
-#' @method cliffDelta dataframe
+#' @method cliffDelta data.frame
+#' @export
 cliffDelta.data.frame <- cliffDelta.matrix
+
 
 ####################################################################################################
 #' Calculate odds ratio
 #'
-#' @param xpos Group 'x' responders
-#' @param xneg Group 'x' non responders
-#' @param ypos Group 'y' responders
-#' @param yneg Group 'y' non responders
+#' @rdname cramerV
+#'
+#' @description Calculate effect sizes using Cramer's V values for pairwise categorical variable
+#' comparisons (e.g. comparisons that would be done by a fisher's exact test)
+#'
+#' @param case.true Case group responders
+#' @param case.false Case group non responders
+#' @param ctrl.true Control group responders
+#' @param ctrl.false Control group non responders
+#' @param show.sign Add `-` to values where effect decreases?
+#'
+#' @return A numeric vector
+#' @export
+#'
+cramerV <- function (x, ...) {
+   UseMethod("cramerV", x)
+}
+
+#' @rdname cramerV
+#' @method cramerV default
+#' @export
+cramerV.default <- function(case.true, case.false, ctrl.true, ctrl.false, show.sign=T){
+   # if(F){
+   #    case.true= c(290 ,60)
+   #    case.false=c(289 ,247)
+   #    ctrl.true= c(3   ,0)
+   #    ctrl.false=c(5034,5309)
+   # }
+
+   ## The 2x2 contigency matrix looks like this
+   ## case.true   ctrl.true
+   ## case.false  ctrl.false
+
+   ## Calculate expect values --------------------------------
+   ## Based on this tutorial: http://www.sthda.com/english/wiki/chi-square-test-of-independence-in-r
+   ## From chisq.test()
+   # x <- matrix(c(case.true[1], case.false[1], ctrl.true[1], ctrl.false[1]), nrow=2)
+   # n <- sum(x)
+   # sr <- rowSums(x)
+   # sc <- colSums(x)
+   # E <- outer(sr, sc, "*")/n
+
+   ## Vectorized version
+   observed <- cbind(case.true, case.false, ctrl.true, ctrl.false)
+   n <- rowSums(observed)
+
+   rowsums1 <- case.true  + ctrl.true
+   rowsums2 <- case.false + ctrl.false
+   colsums1 <- case.true  + case.false
+   colsums2 <- ctrl.true  + ctrl.false
+
+   expected <- cbind(
+      case.true  = rowsums1*colsums1,
+      case.false = rowsums2*colsums1,
+      ctrl.true  = rowsums1*colsums2,
+      ctrl.false = rowsums2*colsums2
+   ) / n
+
+   ## Chi-squared statistic
+   chi2 <- rowSums(
+      (observed - expected)^2 / expected
+   )
+
+   ## Calculate Cramers V --------------------------------
+   ## Based on this tutorial: https://www.real-statistics.com/chi-square-and-f-distributions/effect-size-chi-square/
+   ## V = sqrt(chi2/(n*df))
+   ## where df* = min(r – 1, c – 1) and r = the number of rows and c = the number of columns in the contingency table.
+   V <- sqrt(chi2/n)
+   V[is.na(V)] <- 0
+
+   if(show.sign){
+      ## Add sign
+      observed_1 <- observed + 1
+      ratio_case <- observed_1[,'case.true'] / observed_1[,'case.false']
+      ratio_ctrl <- observed_1[,'ctrl.true'] / observed_1[,'ctrl.false']
+      log_odds <- log2(ratio_case / ratio_ctrl)
+      V[log_odds<0] <- -V[log_odds<0]
+   }
+
+   return(V)
+}
+
+#' @rdname cramerV
+#' @method cramerV matrix
+#' @export
+cramerV.matrix <- function(m){
+   if(ncol(m)!=4){ stop('Input matrix must have 4 columns corresponding to a flattened contingency matrix') }
+   cramerV.default(m[,1],m[,2],m[,3],m[,4])
+}
+
+#' @rdname cramerV
+#' @method cramerV data.frame
+#' @export
+cramerV.data.frame <- cramerV.matrix
+
+
+####################################################################################################
+#' Calculate odds ratio
+#'
+#' @param case.true Case group responders
+#' @param case.false Case group non responders
+#' @param ctrl.true Control group responders
+#' @param ctrl.false Control group non responders
 #' @param trans.method Can be 'none', 'linear' or 'logistic'
 #' @param logistic.growth Steepness/gradient of the logistic function when trans.method=='logistic'
 #'
@@ -115,7 +218,7 @@ oddsRatio <- function (x, ...) {
 
 #' @rdname oddsRatio
 #' @method oddsRatio default
-oddsRatio.default <- function(xpos,xneg,ypos,yneg, trans.method='none', logistic.growth=0.2){
+oddsRatio.default <- function(case.true, case.false, ctrl.true, ctrl.false, trans.method='none', logistic.growth=0.2){
    # if(F){
    #    xpos=c(50, 0,50, 10,1000)
    #    xneg=c(60, 0,0,  20,1001)
@@ -124,8 +227,8 @@ oddsRatio.default <- function(xpos,xneg,ypos,yneg, trans.method='none', logistic
    # }
 
    ## Calc preliminary odds ratios
-   x <- xpos/xneg
-   y <- ypos/yneg
+   x <- case.true/case.false
+   y <- ctrl.true/ctrl.false
 
    or <- x/y
    or_inv <- y/x
@@ -165,13 +268,15 @@ oddsRatio.default <- function(xpos,xneg,ypos,yneg, trans.method='none', logistic
 
 #' @rdname oddsRatio
 #' @method oddsRatio matrix
+#' @export
 oddsRatio.matrix <- function(m, ...){
    if(ncol(m)!=4){ stop('Input matrix must have 4 columns corresponding to a flattened contingency matrix') }
    oddsRatio.default(m[,1],m[,2],m[,3],m[,4])
 }
 
 #' @rdname oddsRatio
-#' @method oddsRatio dataframe
+#' @method oddsRatio data.frame
+#' @export
 oddsRatio.data.frame <- oddsRatio.matrix
 
 
