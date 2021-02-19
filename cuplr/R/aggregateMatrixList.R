a7230ef5e81d914027bf809e44f63b069d34385c
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-02-19 09:40:46 +0100

    aggregateMatrixList now it's own script (originally in plotPerformance)

---
+++ cuplr/R/aggregateMatrixList.R
@@ -0,0 +1,50 @@
+#' Apply a summary function to a list of matrixes of the same dimensions
+#'
+#' @param l A list of matrices
+#' @param func A summary function (default is mean())
+#' @param as.matrix If TRUE, will return a single matrix with the aggregated values. If FALSE, will
+#' return a melted dataframe.
+#' @param value.name A custom column name for the value column of the melted dataframe
+#'
+#' @export
+#'
+aggregateMatrixList <- function(l, func=function(x){ mean(x, na.rm=T) }, as.matrix=F, value.name=NULL){
+   #l=lapply(cv_out,`[[`,'imp')
+
+   all_cols <- unique(unlist(lapply(l, colnames)))
+   l_melt <- lapply(l, function(i){
+      #i=l[[3]]
+      df <- as.data.frame(i)
+      missing_cols <- all_cols[!(all_cols %in% colnames(df))]
+      df[,missing_cols] <- NA
+      df_melt <- reshape2::melt(as.matrix(df))
+      colnames(df_melt) <- c('class','feature','value')
+
+      return(df_melt)
+   })
+
+   df_pre <- suppressWarnings({
+      Reduce(function(x,y){
+         merge(x,y,by=c(1,2), all=T, sort=F)
+      }, l_melt)
+   })
+
+   df <- df_pre[,c(1,2)]
+   m_values <- as.matrix(df_pre[,-c(1,2)])
+   df$value <- apply(m_values,1,function(i){ func(i) })
+
+
+
+   #m_values <- do.call(cbind, lapply(l_melt,`[[`,'value'))
+   #df <- l_melt[[1]]
+   #df$value <- apply(m_values,1,function(i){ func(i) })
+
+   if(as.matrix){
+      out <- reshape2::acast(df, class ~ feature)
+      out <- out[,colnames(l[[1]])]
+      return(out)
+   } else {
+      if(!is.null(value.name)){ colnames(df)[3] <- value.name }
+      return(df)
+   }
+}
