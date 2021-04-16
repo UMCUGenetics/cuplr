a34277423f474ebf4916c2977966ca73457413d1
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-04-16 14:24:29 +0200

    Added cohort summary stats plot (for determining feature correlation direction)

--- cuplr/R/patientReport.main.R
+++ cuplr/R/patientReport.main.R
@@ -23,8 +23,8 @@
 #' the top cancer types (i.e. 1st to 2nd, 2nd to 3rd, 3rd to 4th, etc) is calculated. If e.g. the
 #' differences between the 2nd to 3rd top cancer types is >=`max.prob.diff`, then details panels are
 #' only shown for the 1st and 2nd top cancer types
-#' @param feat.contrib.scales Should scales be fixed ("fixed", the default), free ("free"), or free
-#' in one dimension ("free_x", "free_y")?
+#' @param rel.heights A numeric vector of length 2 specifying the relative heights of the radar plot
+#' and feature contribution plots
 #'
 #' @return A ggplot grob
 #' @export
@@ -35,15 +35,16 @@ patientReport <- function(
    sample.name=NULL, plot.title=sample.name,
    top.n.pred.classes=3, top.n.features=5,
    low.prob.thres=0.3, max.prob.diff=0.15,
-   feat.contrib.scales='fixed'
+   rel.heights=c(1,1)
 ){
 
    if(F){
-      #subset(metadata$training, training_group!=1)
-      sample.name <- 'XXXXXXXX' ## Prostate
+      sample.name <- 'XXXXXXXX' ## unclear, gastric
+      sample.name <- 'XXXXXXXX' ## Breast
 
-      probs=pred_report$probs_adjusted[sample.name,]
-      feat.contrib=subset(pred_report$feat_contrib, sample==sample.name)
+      probs=pred_report.val$probs_adjusted[sample.name,]
+      feat.contrib=subset(pred_report.val$feat_contrib, sample==sample.name)
+      pred.report=NULL
 
       top.n.pred.classes=3
       top.n.features=5
@@ -98,10 +99,10 @@ patientReport <- function(
       probs_top <- probs_top[1:rle(is_diff)$lengths[1]] ## Select the top n classes where diff is still less than e.g. 0.15
    }
 
-   ## Radar plot --------------------------------
-   p_radar <- (function(){
-      pd <- as.data.frame(t(probs))
-      pd <- cbind(group=1, pd)
+   ## Probs --------------------------------
+   p_probs <- (function(){
+      pd_probs <- as.data.frame(t(probs))
+      pd_probs <- cbind(group=1, pd_probs)
 
       sel_colors <- structure(
          colors_dark[ 1:length(names(probs_top)) ],
@@ -115,7 +116,7 @@ patientReport <- function(
       class_colors[names(sel_colors)] <- sel_colors
 
       ggradar(
-         pd,
+         pd_probs,
          group.point.size=1.5, group.line.width=0.75,
          axis.label.size=3.5, axis.label.offset=1.1,
          axis.label.colors=class_colors,
@@ -128,57 +129,137 @@ patientReport <- function(
          )
    })()
 
+   ### Bar plot
+   # pd_probs <- data.frame(class=names(probs), prob=unname(probs))
+   # pd_probs <- pd_probs[order(-pd_probs$prob),]
+   #
+   # pd_probs$class_2 <- factor(pd_probs$class, rev(unique(pd_probs$class)))
+   #
+   # class_colors <- structure(
+   #    rep('lightgrey',nrow(pd_probs)),
+   #    names=pd_probs$class
+   # )
+   # class_colors[1:top.n.pred.classes] <- colors_dark[1:top.n.pred.classes]
+   #
+   # p_probs <- ggplot(pd_probs, aes(x=class_2, y=prob)) +
+   #    geom_bar(aes(fill=class_2), stat='identity', color='black', size=0.3, show.legend=F) +
+   #    geom_text(aes(y=prob+0.01, label=prob), hjust=0, size=3) +
+   #    scale_fill_manual(values=class_colors) +
+   #    scale_y_continuous(limits=c(0, 1.1), breaks=seq(0,1,0.25), expand=c(0,0), name='Cancer type probability') +
+   #
+   #    coord_flip() +
+   #    theme_bw() +
+   #    theme(
+   #       panel.grid.major.y=element_blank(),
+   #       panel.grid.minor.y=element_blank(),
+   #       panel.grid.minor.x=element_blank(),
+   #       axis.title.y=element_blank()
+   #    )
+
+   ## Polar bar plot
+   # pd_prob <- data.frame(
+   #    pred_class=names(probs),
+   #    prob=unname(probs)
+   # )
+   #
+   # n_classes <- nrow(pd_prob)
+   # pd_prob$index <- 1:n_classes
+   #
+   # pd_prob$pred_class <- factor(pd_prob$pred_class, unique(pd_prob$pred_class))
+   #
+   # ## Labels
+   # pd_prob$angle <- 0:(n_classes-1) * 360/n_classes
+   # pd_prob$label <- pd_prob$pred_class
+   # pd_prob$label_hjust <- 0
+   # pd_prob <- within(pd_prob,{
+   #    label_hjust[round(angle) %in% c(0,180)] <- 0.5
+   #    label_hjust[angle>180] <- 1
+   # })
+   #
+   # plot_rotate_rad <- 0.5 * (360/n_classes) * (pi/180)
+   #
+   # coord_radar <- function (theta = "x", start = 0, direction = 1) {
+   #    theta <- match.arg(theta, c("x", "y"))
+   #    r <- if (theta == "x") "y" else "x"
+   #    ggproto("CordRadar", CoordPolar, theta = theta, r = r, start = start,
+   #            direction = sign(direction),
+   #            is_linear = function(coord) TRUE)
+   # }
+   #
+   # p_probs <- ggplot(pd_prob, aes(x=pred_class, y=prob)) +
+   #
+   #    ## Draw panel grid
+   #    geom_segment(aes(x=pred_class, xend=pred_class, y=0, yend=1), color='lightgrey') +
+   #    geom_hline(data=data.frame(), aes(yintercept=seq(0,1,0.25)), color='lightgrey') +
+   #
+   #    geom_segment(aes(x=pred_class, xend=pred_class, y=0, yend=prob), color='indianred', size=3) +
+   #    geom_text(data=data.frame(x=1, y=seq(0,1,0.25)), aes(x=x, y=y, label=y), vjust=1.1) +
+   #
+   #    #geom_text(aes(y=label_pos, label=label, hjust=label_hjust)) +
+   #    #geom_text(aes(y=1.1, label=pred_class, hjust=label_hjust)) +
+   #    scale_y_continuous(expand=c(0, 0.2, 0, 0), limits=c(0, 1)) +
+   #
+   #    #coord_flip() +
+   #    coord_polar(start = -plot_rotate_rad) +
+   #    #coord_radar(start = -plot_rotate_rad) +
+   #
+   #    theme_bw() +
+   #
+   #    theme(
+   #       panel.grid=element_blank(),
+   #       panel.border=element_blank(),
+   #       axis.text.x=element_text(size=12),
+   #       axis.title.x=element_blank(),
+   #       axis.title.y=element_blank(),
+   #       axis.text.y=element_blank(),
+   #       axis.ticks.y=element_blank()
+   #    )
+
+
    ## Feat contrib --------------------------------
-   p_feat_contrib <- (function(){
+   feat_plots_xlims <- c(1-0.6, top.n.features+0.6)
 
-      pd <- feat.contrib[feat.contrib$binary_rf %in% names(probs_top),]
+   pd_feat <- feat.contrib[feat.contrib$binary_rf %in% names(probs_top),]
 
-      ## Order by top predicted class and decreasing feat contrib
-      pd$binary_rf <- factor(pd$binary_rf, names(probs_top))
-      pd <- pd[order(pd$binary_rf, -pd$contrib),]
+   ## Order by top predicted class and decreasing feat contrib
+   pd_feat$binary_rf <- factor(pd_feat$binary_rf, names(probs_top))
+   pd_feat <- pd_feat[order(pd_feat$binary_rf, -pd_feat$contrib),]
 
-      ## Select top features
-      pd$index <- unlist(lapply(split(1:nrow(pd), pd$binary_rf),function(i){
-         1:length(i)
-      }), use.names=F)
-      pd <- pd[pd$index<=top.n.features,]
+   ## Select top features
+   pd_feat$feature_rank <- unlist(lapply(split(1:nrow(pd_feat), pd_feat$binary_rf),function(i){
+      1:length(i)
+   }), use.names=F)
+   pd_feat <- pd_feat[pd_feat$feature_rank<=top.n.features,]
 
-      ## Add top binary RF probs
-      pd$prob <- probs[ match(pd$binary_rf, names(probs)) ]
-      pd$strip_title <- paste0(pd$binary_rf,' (Probability=',pd$prob,')')
-      pd$strip_title <- factor(pd$strip_title, unique(pd$strip_title))
+   p_contrib <- ggplot(pd_feat, aes(x=-feature_rank, y=contrib)) +
+      coord_flip() +
+      facet_grid(binary_rf~., scales='fixed', switch='y') +
 
-      ## Add feature supplement to name
-      pd$feature_supplement <- paste0(' (',pd$feature_supplement,')')
-      pd$feature_supplement[pd$feature_supplement==' ()'] <- ''
-      pd$label <- paste0(as.character(pd$feature),pd$feature_supplement)
+      geom_bar(stat='identity', fill='lightgrey', color='darkgrey', size=0.3, width=0.8) +
+      geom_text(aes(y=0, label=feature), size=3.5, angle=0, hjust=0) +
+      ylab('Feature contribution') +
+      scale_x_continuous(expand=c(0, 0), limits=-rev(feat_plots_xlims)) +
 
-      ## Main
-      p <- ggplot(pd, aes(x=-index, y=contrib)) +
-         coord_flip() +
-         facet_wrap(~strip_title, ncol=1, scales=feat.contrib.scales) +
+      theme_bw() +
+      theme(
+         panel.grid.major.y=element_blank(),
+         #panel.grid.minor.y=element_blank(),
+         panel.grid.minor.x=element_blank(),
+         axis.title.y=element_blank(),
+         axis.text.y=element_blank(),
+         axis.ticks.y=element_blank()
+      )
 
-         geom_bar(stat='identity', fill='lightgrey', color='darkgrey', size=0.3) +
-         geom_text(aes(y=0, label=label), size=3.5, angle=0, hjust=0) +
-         ylab('Feature contribution') +
 
-         theme_bw() +
-         theme(
-            panel.grid.major.y=element_blank(),
-            panel.grid.minor.y=element_blank(),
-            panel.grid.minor.x=element_blank(),
-            axis.title.y=element_blank(),
-            axis.text.y=element_blank(),
-            axis.ticks.y=element_blank()
-         )
-
-      ## Edit strip colors
+   ## Edit strip colors
+   colorizeStrips <- function(p){
+      #p=p_contrib
       g <- ggplot_gtable(ggplot_build(p))
       strip_both <- which(grepl('strip-', g$layout$name))
-      fills <- rev(structure(
+      fills <- structure(
          colors_light[ 1:length(names(probs_top)) ],
          names=names(probs_top)
-      ))
+      )
 
       counter <- 1
       for (i in strip_both) {
@@ -187,12 +268,121 @@ patientReport <- function(
          counter <- counter+1
       }
       return(g)
-   })()
+   }
 
-   out <- gridExtra::arrangeGrob(
-      p_radar,
-      p_feat_contrib,
-      nrow=1, widths=c(2, 1)
+   p_contrib <- colorizeStrips(p_contrib)
+
+   ## Cohort summary stats --------------------------------
+   feat_meta_colnames <- c('binary_rf','feature','feature_rank')
+   feat_stat_colnames <- c('avg_case','avg_ctrl','value')
+
+   pd_feat_avg <- reshape2::melt(
+      pd_feat[,c(feat_meta_colnames, feat_stat_colnames)],
+      id.vars=feat_meta_colnames
    )
-   return(out)
+
+   ## Center data based on the avg values of the ctrl cohort
+   norm_stats <- t(apply(
+      pd_feat[,feat_stat_colnames],
+      1,
+      function(x){ (x-min(x))/(max(x)-min(x)) }
+   ))
+   norm_stats <- as.data.frame(norm_stats)
+   shift_factor <- 1 - norm_stats$avg_ctrl
+   norm_stats <- norm_stats + shift_factor
+
+   colnames(norm_stats) <- paste0('norm.',colnames(norm_stats))
+   norm_stats <- cbind(pd_feat[,feat_meta_colnames], norm_stats)
+   norm_stats <- reshape2::melt(norm_stats,id.vars=feat_meta_colnames)
+
+   pd_feat_avg$value_norm <- norm_stats$value
+   rm(norm_stats)
+
+   ## Stagger lollipops
+   dodge_width <- 0.2
+   pd_feat_avg$xpos <- pd_feat_avg$feature_rank
+   pd_feat_avg$xpos[pd_feat_avg$variable=='avg_case'] <- pd_feat_avg$xpos[pd_feat_avg$variable=='avg_case'] + dodge_width
+   pd_feat_avg$xpos[pd_feat_avg$variable=='value'] <- pd_feat_avg$xpos[pd_feat_avg$variable=='value'] - dodge_width
+
+   ## Reverse x values for coord_flip
+   pd_feat_avg$xpos <- -pd_feat_avg$xpos
+
+   ## Calc label position
+   label_offset <- 0.05
+   pd_feat_avg$label_ypos <- pd_feat_avg$value_norm
+   pd_feat_avg$label_hjust <- 1
+
+   pd_feat_avg <- within(pd_feat_avg,{
+      label_ypos[label_ypos >1] <- label_ypos[label_ypos >1] + label_offset
+      label_ypos[label_ypos<=1] <- label_ypos[label_ypos<=1] - label_offset
+      label_hjust[label_ypos>1] <- 0
+   })
+
+   ## Reorder value type
+   pd_feat_avg$variable <- factor(pd_feat_avg$variable, c('value','avg_case','avg_ctrl'))
+   value_type_names <- c('Sample','Mean, cancer type','Mean, other')
+
+   ## Format label
+   pd_feat_avg$value <- signif(pd_feat_avg$value,3)
+   #log10(abs(pd_feat_avg$value))
+   #as.character(pd_feat_avg$value)
+
+   p_feat_avg <- ggplot(pd_feat_avg, aes(x=xpos, y=value_norm)) +
+      facet_grid(binary_rf~., scales='fixed') +
+
+      ## Baseline
+      geom_segment(
+         data=subset(pd_feat_avg, variable=='avg_ctrl'),
+         mapping=aes(x=xpos-dodge_width-0.1, xend=xpos+dodge_width+0.1, y=1, yend=1),
+         show.legend=F
+      ) +
+
+      ## Data points
+      geom_segment(aes(x=xpos, xend=xpos, y=1, yend=value_norm, color=variable), show.legend=F) +
+      geom_point(aes(shape=variable, size=variable, color=variable), show.legend=T) +
+      geom_text(aes(y=label_ypos, label=value, hjust=label_hjust, color=variable), show.legend=F, size=2.5) +
+
+      ## Legend
+      scale_color_manual(values=c(avg_ctrl='black',avg_case='darkgrey',value='#F58225'), labels=value_type_names) +
+      scale_shape_manual(values=c(avg_ctrl=124,avg_case=16,value=16), labels=value_type_names) +
+      scale_size_manual(values=c(avg_ctrl=0,avg_case=2,value=2), labels=value_type_names) +
+      guides(shape=F, size=F) +
+
+      ## Axes
+      scale_y_continuous(expand=c(0.1, 0.1), name='Feature value (variable units)') +
+      scale_x_continuous(expand=c(0, 0), limits=-rev(feat_plots_xlims)) +
+
+      coord_flip() +
+      theme_bw() +
+      theme(
+         panel.grid.major.y=element_blank(),
+         #panel.grid.minor.y=element_line(color='grey'),
+         panel.grid.major.x=element_blank(),
+         panel.grid.minor.x=element_blank(),
+         axis.title.y=element_blank(),
+         axis.text.y=element_blank(),
+         axis.ticks.y=element_blank(),
+         axis.text.x=element_blank(),
+         axis.ticks.x=element_blank(),
+         legend.position='bottom',
+         legend.title=element_blank(),
+         legend.direction='horizontal',
+         strip.background = element_blank(),
+         strip.text = element_blank()
+      )
+
+   #p_feat_avg <- colorizeStrips(p_feat_avg)
+
+   ## Merge plots --------------------------------
+   cowplot::plot_grid(
+      p_probs,
+      cowplot::plot_grid(
+         p_contrib, p_feat_avg,
+         nrow=1, align='h', axis='tblr'
+      ),
+      ncol=1,
+      rel_heights=rel.heights
+   )
+
+
 }
