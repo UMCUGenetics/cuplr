7be132e623bb84f39dd9e453c90b22ac66fb7476
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-05-13 16:46:15 +0200

    Removed deprecated model

--- cuplr/R/aaa.R
+++ cuplr/R/aaa.R
@@ -1,24 +1,24 @@
 ## Load default paths on package load ================================
 
-.onLoad <- function(libname, pkgname){
-
-   ## Paths within the package
-   pkg_constant_paths <- list(
-      c('CUPLR','/model.rds')
-   )
-
-   for(i in pkg_constant_paths){
-      assign(
-         i[1], system.file(i[2], package='cuplr'),
-         envir=parent.env(environment())
-      )
-   }
-}
-
-if(F){
-   file.copy(
-      '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.09c_originalFeatures/model.rds',
-      '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/inst/model.rds'
-   )
-}
+# .onLoad <- function(libname, pkgname){
+#
+#    ## Paths within the package
+#    pkg_constant_paths <- list(
+#       c('CUPLR','/model.rds')
+#    )
+#
+#    for(i in pkg_constant_paths){
+#       assign(
+#          i[1], system.file(i[2], package='cuplr'),
+#          envir=parent.env(environment())
+#       )
+#    }
+# }
+#
+# if(F){
+#    file.copy(
+#       '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.09c_originalFeatures/model.rds',
+#       '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/inst/model.rds'
+#    )
+# }
 
