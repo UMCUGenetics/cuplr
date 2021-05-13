97a2b4473cd8831c6f90cddcb0b217ce9aa60f29
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-05-13 16:41:17 +0200

    In cacheAndReadData(), updated extension handling and hash generation for local file path

--- commonUtils/R/misc.R
+++ commonUtils/R/misc.R
@@ -128,24 +128,46 @@ as.factor.data.frame <- function(df, levels, ...){
 #'
 #' @param remote.path Remote path to txt or rds file
 #' @param local.path Local path to copy to
+#' @param cache.dir Path to the cache directory. If `local.path` is not specified, files will be
+#' cached here.
 #' @param overwrite Redownload data from remote?
 #'
 #' @return A dataframe
 #' @export
 #'
-cacheAndReadData <- function(remote.path, local.path=NULL, overwrite=F){
+cacheAndReadData <- function(
+   remote.path, local.path=NULL,
+   cache.dir=path.expand('~/Documents/R_cache/'),
+   overwrite=F
+){
 
    #remote.path=paste0(base_dir,'/misc/processed/Chromatin_modifiers/scripts/annotate_svs/vis_sv_data_compact.txt.gz')
+
+   ## Init --------------------------------
+   ext <- c('.rds','.txt','.txt.gz','.csv','.csv.gz')
+   regex <- gsub('[.]','[.]',ext)
+   regex <- paste0(regex,'$')
+
+   is_valid_ext <- sapply(regex, function(i){ grepl(i, remote.path) })
+   if(all(!is_valid_ext)){
+      stop('File extension must be one of the following:\n  ', paste(ext, collapse=', '))
+   }
+
+   ext <- ext[ which(is_valid_ext) ]
+   regex <- regex[ which(is_valid_ext) ]
+
    set.seed(nchar(remote.path))
 
+   ## Copy --------------------------------
    if(is.null(local.path)){
       local.path <- paste0(
-         '/Users/lnguyen/Documents/R_cache/',
-         paste(sample(letters, 8), collapse=''),
-         '_',basename(remote.path)
+         cache.dir,
+         sub(regex,'',basename(remote.path)),
+         '.',paste(sample(letters, 8), collapse=''),ext
       )
    }
 
+   local.path <- gsub('/+', '/', local.path)
    if(!file.exists(local.path) | overwrite){
       if(!file.exists(local.path)){
          message('Making local copy: ', local.path)
@@ -159,11 +181,15 @@ cacheAndReadData <- function(remote.path, local.path=NULL, overwrite=F){
          local.path
       ))
    }
+
+   ## Read --------------------------------
    message('Reading local copy: ', local.path)
-   if(grepl('.rds$',local.path)){
+   if(ext=='.rds'){
       readRDS(local.path)
-   } else {
+   } else if(grepl('txt',ext)){
       read.delim(local.path, check.names=F)
+   } else {
+      read.csv(local.path, check.names=F)
    }
 }
 
