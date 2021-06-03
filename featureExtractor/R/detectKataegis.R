5e2577a5dc1aa46f4a2d21e805e2980ac64f97d9
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-03 11:57:03 +0200

    - Use mutSigExtractor::variantsFromVcf() for loading vcfs\n- Fixed exception handling when input contains no rows\n- Removed deprecated functions from R script

--- featureExtractor/R/detectKataegis.R
+++ featureExtractor/R/detectKataegis.R
@@ -32,8 +32,8 @@
 #'
 detectKataegis <- function(
    ## mutSigExtractor::variantsFromVcf) args
-   vcf.file=NULL, df=NULL, vcf.filter='PASS', keep.chroms=c(1:22,'X'), 
-   merge.consecutive=F,
+   vcf.file=NULL, df=NULL, sample.name=NULL, vcf.filter='PASS', keep.chroms=c(1:22,'X'),
+   ref.genome=mutSigExtractor::DEFAULT_GENOME, #clonal.variants.only=T,
    
    ## PCF/kataegis detection parameters
    min.muts=6, imd.thres=1000, cp=1E-6, 
@@ -41,41 +41,32 @@ detectKataegis <- function(
    
    output.type='count', verbose=T, ...
 ){
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/181206_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data//somatics/171002_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/180808_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz' ## Hypermutator n_kat=1600
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/170311_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz' ## Medium hypermutator k_kat=200
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104-update3//somatics/160604_HMFreg0056_FR10302045_FR10302046_XXXXXXXX/purple/XXXXXXXX.purple.somatic.vcf.gz' ## Job fail
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/PCAWG/pipeline5/per-donor//DO217817-from-jar//purple25/DO217817T.purple.somatic.vcf.gz' ## Job fail
-   # vcf.filter='PASS'
-   # keep.chroms=c(1:22,'X')
-   # merge.consecutive=F
-   # min.muts=6
-   # imd.thres=1000
-   # cp=1E-5
-   # 
-   # verbose=T
-   
+
    if(!is.null(vcf.file)){
-      df <- vcfAsDataframe(
-         vcf.file, vcf.filter='PASS', keep.chroms=keep.chroms, 
-         merge.consecutive=merge.consecutive, verbose=verbose
-      )
       
-   } else if(!is.null(df)){
-      if(c('chrom','pos','ref','alt') %in% colnames(df)){
-         stop("`df` must have the colnames: chrom, pos, ref, alt")
+      df <- mutSigExtractor::variantsFromVcf(
+         vcf.file, vcf.fields=c(1,2,4,5,7,8),
+         vcf.filter=vcf.filter, keep.chroms=keep.chroms,
+         ref.genome=ref.genome, verbose=verbose,
+         ...
+      )
+   }
+   if(is.null(df)){ stop("Input must be specified to either `vcf.file` or `df`") }
+   
+   if(nrow(df)!=0){
+      df_colnames <- c('chrom','pos','ref','alt')
+      if(!(identical(colnames(df)[1:4], df_colnames))){
+         warning("colnames(df)[1:4] != c('chrom','pos','ref','alt'). Assuming first 4 columns are these columns")
+         colnames(df)[1:4] <- df_colnames
       }
-   } else {
-      stop("Input must be specified to either `vcf.file` or `df`")
+      
+      if(verbose){ message('Removing rows with multiple ALT sequences...') }
+      df <- df[!grepl(',',df$alt),]
+      
+      if(verbose){ message('Subsetting for SNVs...') }
+      df <- df[nchar(df$ref)==1 & nchar(df$alt)==1,]
    }
    
-   if(verbose){ message('Removing rows with multiple ALT sequences...') }
-   df <- df[!grepl(',',df$alt),]
-   
-   if(verbose){ message('Subsetting for SNVs...') }
-   df <- df[nchar(df$ref)==1 & nchar(df$alt)==1,]
-
    if(nrow(df)==0){
       if(output.type=='count'){
          return(0)
@@ -88,6 +79,14 @@ detectKataegis <- function(
    df$chrom <- factor(df$chrom, unique(df$chrom))
    df <- df[order(df$chrom, df$pos),]
    df_split <- split(df[,c('chrom','pos')], df$chrom)
+   
+   ## Helper function
+   rle2Clusters <- function(x){
+      rle_out <- rle(x)
+      unlist(lapply(1:length(rle_out$lengths), function(i){
+         rep(i, rle_out$lengths[i])
+      }))
+   }
    
    imd_pcf <- do.call(rbind, lapply(df_split, function(i){
       #i=df_split[[3]]
@@ -200,153 +199,4 @@ detectKataegis <- function(
       stop("`output.type` must be one of the following: 'count', 'kat_segments', 'imd_kat', 'imd_all'")
    }
 }
-
-####################################################################################################
-#' Make kataegis contexts
-#'
-#' @param vcf.snv Path to the vcf file containing SNVs
-#' @param vcf.sv Path to the GRIDSS vcf file containing
-#' @param vcf.filter Which variants to filter for? Default is 'PASS'. Only applies if vcf.file is
-#' specified
-#' @param keep.chroms Which chromosomes to keep? Default is c(1:22,'X')
-#' @param merge.consecutive Some vcfs report MNVs as consecutive variants. For these vcfs, such rows
-#' @param kat.foci.flank.length Number of bp on either side of kataegis to scan for SVs
-#' @param verbose Show progress messages?
-#'
-#' @return An integer vector
-#' @export
-#'
-extractContextsKataegis <- function(
-   vcf.snv, vcf.sv, vcf.filter='PASS', keep.chroms=c(1:22,'X'), merge.consecutive=F, 
-   kat.foci.flank.length=1000, verbose=T
-){
-   # vcf.snv='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/181206_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # vcf.sv='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/181206_HMFregXXXXXXXX/XXXXXXXX.purple.sv.ann.vcf.gz'
-   
-   # vcf.snv='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/161201_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # vcf.sv='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/161201_HMFregXXXXXXXX/XXXXXXXX.purple.sv.ann.vcf.gz'
-   
-   counts <- structure(
-      rep(0,2),
-      names=c('kataegis.sv_pos','kataegis.sv_neg')
-   )
-   
-   if(verbose){ message('## Detecting kataegis') }
-   kat_snv <- detectKataegis(
-      vcf.snv, output.type='kat_segments', 
-      vcf.filter=vcf.filter, keep.chroms=keep.chroms,
-      merge.consecutive=merge.consecutive,
-      verbose=verbose
-   )
-   
-   if(nrow(kat_snv)==0){ 
-      if(verbose){ message('No kataegis events found. Returning 0s') }
-      return(counts) 
-   }
-   
-   kat_snv$pos_linear <- linearizeChromPos(kat_snv$chrom, kat_snv$pos)
-   
-   if(verbose){ message('\n## Loading SVs') }
-   variants_sv <- vcfAsDataframe(
-      vcf.sv, 
-      #vcf.fields=c('CHROM','POS','REF','ALT','FILTER','ID','INFO'),
-      vcf.filter=vcf.filter, keep.chroms=keep.chroms, 
-      verbose=verbose
-   )
-   
-   if(verbose){ message('\n## Making kataegis contexts') }
-   if(verbose){ message('Identifying unique SV breakends...') }
-   if(nrow(variants_sv)!=0){
-      uniq_breakends <- (function(){
-         l <- regmatches(variants_sv$alt, gregexpr('\\d|\\w+:\\d+', variants_sv$alt))
-         df <- as.data.frame(do.call(rbind, lapply(l, function(i){
-            if(length(i) == 0){ c(NA,NA) }
-            else { unlist(strsplit(i, ':')) }
-         })))
-         colnames(df) <- c('chrom','pos')
-         df$chrom <- paste0('chr',df$chrom)
-         
-         df <- rbind(
-            variants_sv[,c('chrom','pos')],
-            df
-         )
-         
-         df <- na.exclude(df)
-         df <- unique(df)
-         df$pos <- as.integer(df$pos)
-         #df$chrom <- factor(df$chrom, levels=unique(variants_sv$chrom))
-         #df <- df[order(df$chrom, df$pos),]
-         
-         return(df)
-      })()
-      
-      uniq_breakends$pos_linear <- linearizeChromPos(uniq_breakends$chrom, uniq_breakends$pos)
-      uniq_breakends <- uniq_breakends[order(uniq_breakends$pos_linear),]
-      uniq_breakends <- uniq_breakends[uniq_breakends$chrom %in% kat_snv$chrom,]
-   } else {
-      uniq_breakends <- data.frame()
-   }
-   
-   if(verbose){ message('Detecting nearby breakends to kataegis foci...') }
-   has_nearby_breakend <- (function(){
-      l <- split(kat_snv, kat_snv$segment_id)
-      sapply(l, function(i){
-         #i=l[[1]]
-         if(nrow(uniq_breakends)!=0){
-            boundary_l <- min(i$pos_linear) - kat.foci.flank.length
-            boundary_r <- max(i$pos_linear) + kat.foci.flank.length
-            any(boundary_l <= uniq_breakends$pos_linear & uniq_breakends$pos_linear <= boundary_r)
-         } else {
-            FALSE
-         }
-      })
-   })()
-   
-   counts['kataegis.sv_pos'] <- sum(has_nearby_breakend)
-   counts['kataegis.sv_neg'] <- sum(!has_nearby_breakend)
-   
-   return(counts)
-}
-
-####################################################################################################
-plotRainfall <- function(imd){
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/181206_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/180808_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz' ## Hyper mutator
-   # vcf.file='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/180224_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
-   # imd <- detectKataegis(
-   #    vcf.file,
-   #    output.type=2
-   # )
-   
-   imd$chrom <- gsub('chr','',imd$chrom)
-   imd$chrom <- factor(imd$chrom, unique(imd$chrom))
-   #imd$is_kat_locus <- factor(imd$is_kat_locus, levels=c('FALSE','TRUE'))
-   
-   require(ggplot2)
-   ggplot(imd, aes(x=pos, y=log10(imd))) +
-      facet_grid(~chrom, space='free_x', scales='free_x') +
-      geom_point(aes(color=is_kat_locus), size=0.5) +
-      scale_color_manual(values=c('TRUE'='black','FALSE'='grey'), guide=F) +
-      theme_bw() +
-      theme(
-         axis.title.x=element_blank(),
-         axis.text.x=element_blank(),
-         axis.ticks.x=element_blank(),
-         panel.spacing=unit(0, "lines"),
-         panel.grid.major.x=element_blank(),
-         panel.grid.minor.x=element_blank()
-      )
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
