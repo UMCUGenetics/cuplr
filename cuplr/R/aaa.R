34809d81e819c95852964a2ced28b2e13e045b6f
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-03 12:10:02 +0200

    Added paths to classifier

--- cuplr/R/aaa.R
+++ cuplr/R/aaa.R
@@ -1,24 +1,34 @@
 ## Load default paths on package load ================================
 
-# .onLoad <- function(libname, pkgname){
-#
-#    ## Paths within the package
-#    pkg_constant_paths <- list(
-#       c('CUPLR','/model.rds')
-#    )
-#
-#    for(i in pkg_constant_paths){
-#       assign(
-#          i[1], system.file(i[2], package='cuplr'),
-#          envir=parent.env(environment())
-#       )
-#    }
-# }
-#
-# if(F){
-#    file.copy(
-#       '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.09c_originalFeatures/model.rds',
-#       '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/inst/model.rds'
-#    )
-# }
+.onLoad <- function(libname, pkgname){
+
+   ## Paths within the package
+   pkg_constant_paths <- list(
+      c('CUPLR','/model.rds')
+   )
+
+   for(i in pkg_constant_paths){
+      assign(
+         i[1], system.file(i[2], package='cuplr'),
+         envir=parent.env(environment())
+      )
+   }
+}
+
+if(F){
+   model_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.16c_featureExtractor/'
+   pkg_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/'
+
+   file.copy(
+      paste0(model_dir,'/final/model.rds'),
+      paste0(pkg_dir,'/cuplr/inst/model.rds'),
+      overwrite=T
+   )
+
+   file.copy(
+      paste0(model_dir,'/report/prob_calib_curves.txt'),
+      paste0(pkg_dir,'/cuplr/inst/prob_calib_curves.txt'),
+      overwrite=T
+   )
+}
 
