cd6fcb49011847fb4619c784d51005e1388c01aa
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-03 12:01:41 +0200

    Removed paths to deprecated whitelists and lookup tables

--- featureExtractor/R/aaa.R
+++ featureExtractor/R/aaa.R
@@ -3,19 +3,9 @@
 
    ## Paths within the package
    pkg_constant_paths <- list(
-      c('CYTOBANDS','/cytoBand.txt.gz'),
-      
-      c('VIRAL_HOST_REF','/viral_host_ref_ann.txt'),
       c('GENE_FUSION_WHITELIST','/gene_fusion_whitelist.txt'),
       
-      c('GENE_AMP_WHITELIST','/gene_amp_whitelist.txt'),
-      c('GENE_DEF_WHITELIST','/gene_def_whitelist.txt'),
-      
-      c('REP_ELEM_WHITELIST','/rep_elem_whitelist.txt'),
-      
-      c('RESOLVED_TYPE_ANNOTATIONS','/resolved_type_annotations.txt'),
-      
-      c('GENOME_BINS','/genome_bins.txt.gz')
+      c('GENE_DRIVER_WHITELIST','/gene_driver_whitelist.txt')
    )
 
    for(i in pkg_constant_paths){
@@ -24,8 +14,4 @@
          envir=parent.env(environment())
       )
    }
-}
-
-if(F){
-   devtools::load_all('/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn//CUPs_classifier/processed/cuplr/featureExtractor/')
 }
