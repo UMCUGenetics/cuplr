9def34d525a26e6e9d2100de5be3d358f623d57a
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-03 16:59:20 +0200

    Moved colMeansTrimmed() to separate script from univarFeatSel()

---
+++ statsExtra/R/colMeansTrimmed.R
@@ -0,0 +1,21 @@
+#' Calculate trimmed means by column
+#'
+#' @param m A numeric matrix
+#' @param trim The fraction (0 to 0.5) of observations to be trimmed from each end of `m` before the
+#' mean is computed. Values of trim outside that range are taken as the nearest endpoint
+#'
+#' @return A numeric vector
+#' @export
+#'
+colMeansTrimmed <- function(m, trim=0.25, na.rm=F){
+   if(!is.matrix(m) | !is.numeric(m)){ stop('`m` must be a numeric matrix') }
+
+   m <- apply(m,2,sort)
+
+   n <- nrow(m)
+   lo <- floor(n * trim) + 1
+   hi <- n + 1 - lo
+
+   m <- m[lo:hi,,drop=F]
+   colMeans(m, na.rm=na.rm)
+}
