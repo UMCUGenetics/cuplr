2578cce6043aec2c1b37059e7689b10c150f21cf
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-03 11:50:38 +0200

    Removed assignment of output before returning

--- featureExtractor/R/getPurplePurityData.R
+++ featureExtractor/R/getPurplePurityData.R
@@ -9,11 +9,9 @@ getPurplePurityData <- function(purple.purity.path, tag.features=T){
    #purple.purity.path='/Users/lnguyen/hpc/cuppen/shared_resources/HMF_data/DR-104/data/somatics/160704_HMFregCPCT_FR10301986_FR12244591_CPCT02020306/CPCT02020306T.purple.purity.tsv'
    df <- read.delim(purple.purity.path, check.names=F, stringsAsFactors=F)
 
-   out <- c(
+   c(
       gender = df$gender=='FEMALE', ## MALE_KLINEFELTER becomes male
       has_wgd = as.logical(df$wholeGenomeDuplication)
    )
-   
-   return(out)
 }
 
