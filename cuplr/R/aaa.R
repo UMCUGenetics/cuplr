fc6c857f35f7ba347c92951ae43e1b7d63133454
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-05 01:20:11 +0200

    Commented out file copying code

--- cuplr/R/aaa.R
+++ cuplr/R/aaa.R
@@ -4,7 +4,8 @@
 
    ## Paths within the package
    pkg_constant_paths <- list(
-      c('CUPLR','/model.rds')
+      c('MODEL_PATH','/model.rds'),
+      c('PROB_CAL_CURVES_PATH','/prob_calib_curves.txt')
    )
 
    for(i in pkg_constant_paths){
@@ -15,20 +16,20 @@
    }
 }
 
-if(F){
-   model_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.16c_featureExtractor/'
-   pkg_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/'
-
-   file.copy(
-      paste0(model_dir,'/final/model.rds'),
-      paste0(pkg_dir,'/cuplr/inst/model.rds'),
-      overwrite=T
-   )
-
-   file.copy(
-      paste0(model_dir,'/report/prob_calib_curves.txt'),
-      paste0(pkg_dir,'/cuplr/inst/prob_calib_curves.txt'),
-      overwrite=T
-   )
-}
+# if(F){
+#    model_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.16e_newLabels/'
+#    pkg_dir <- '/Users/lnguyen/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/'
+#
+#    file.copy(
+#       paste0(model_dir,'/final/model.rds'),
+#       paste0(pkg_dir,'/cuplr/inst/model.rds'),
+#       overwrite=T
+#    )
+#
+#    file.copy(
+#       paste0(model_dir,'/report/prob_calib_curves.txt'),
+#       paste0(pkg_dir,'/cuplr/inst/prob_calib_curves.txt'),
+#       overwrite=T
+#    )
+# }
 
