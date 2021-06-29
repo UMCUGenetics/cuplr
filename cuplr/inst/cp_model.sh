dd1d6cf786c4bcae5769a6e5429f202aabd610fb
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-06-29 12:29:48 +0200

    Added script to copy random forest models to inst/

---
+++ cuplr/inst/cp_model.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+out_dir=/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/inst/
+model_dir=/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.17a_noClonalSplit/
+
+cp $model_dir/final/model.rds $out_dir/model.rds
+cp $model_dir/report/prob_calib_curves.txt $out_dir/prob_calib_curves.txt
