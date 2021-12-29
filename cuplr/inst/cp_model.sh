37f40778d4cf70fe3901b56051b6b277b4151324
Luan Nguyen <nguyen.n.luan777@gmail.com>
2021-12-29 14:51:56 +0100

    Updated model:\n- Added Sarcoma_Osteo and NET_Lung classifiers\n- Training set no longer contains duplicate biopsies per patient\n- Numerous changes to features used

--- cuplr/inst/cp_model.sh
+++ cuplr/inst/cp_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 out_dir=/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/inst/
-model_dir=/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.18a_newLabels/
+model_dir=/hpc/cuppen/projects/P0013_WGS_patterns_Diagn/CUPs_classifier/processed/cuplr/cuplr/models/0.21a_DR104-update5_pcawgSoftFilt2/
 
 cp $model_dir/final/model.rds $out_dir/model.rds
 cp $model_dir/report/prob_calib_curves.txt $out_dir/prob_calib_curves.txt
