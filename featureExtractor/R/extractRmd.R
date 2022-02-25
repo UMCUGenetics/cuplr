256689a4dda00ef8e6ee6503a273c2980dfd1534
Luan Nguyen <nguyen.n.luan777@gmail.com>
2022-02-25 11:13:38 +0100

    - `genome.bins` argument now always needs to be provided. `bin.size` argument removed\n- Optimized assignment of RMD bin by using integer representation of chrom:pos coords

--- featureExtractor/R/extractRmd.R
+++ featureExtractor/R/extractRmd.R
@@ -7,9 +7,8 @@
 #' matrix will be named to this. If none is provided, the basename of the vcf
 #' file will be used.
 #' @param genome.bins A dataframe containing predetermined bins. Must contain the columns: chrom, 
-#' start, end, chrom_arm, start_linear, end_linear, bin_name
-#' @param bin.size Alternatively if `genome.bins` is NULL, the user can specify the size of the
-#' genomic bins
+#' start, end, bin_name. Alternatively, if a path to a txt (as a string) is provided, the txt will
+#' be loaded.
 #' @param as.matrix Return a matrix or vector?
 #' @param vcf.filter A character or character vector to specifying which variants to keep,
 #' corresponding to the values in the vcf FILTER column
@@ -25,8 +24,8 @@
 #' @export
 #'
 extractRmd <- function(
-   vcf.file=NULL, df=NULL, sample.name=NULL, genome.bins=NULL, bin.size=1e6, as.matrix=T,
-   vcf.filter='PASS', clonal.variants.only=T, mut.types='snv',
+   vcf.file=NULL, df=NULL, sample.name=NULL, genome.bins=RMD_BINS_PATH, as.matrix=T,
+   vcf.filter='PASS', clonal.variants.only=F, mut.types='snv',
    verbose=F, ...
 ){
    # if(F){
@@ -34,18 +33,24 @@ extractRmd <- function(
    #    vcf.file='/Users/lnguyen//hpc/cuppen/shared_resources/HMF_data/DR-104/data//somatics/200117_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
    #    vcf.file='/Users/lnguyen//hpc/cuppen/shared_resources/HMF_data/DR-104/data//somatics/160825_HMFregXXXXXXXX/XXXXXXXX.purple.somatic.vcf.gz'
    #    vcf.file='/Users/lnguyen//hpc/cuppen/shared_resources/PCAWG/pipeline5/per-donor//DO217817-from-jar//purple25/DO217817T.purple.somatic.vcf.gz'
-   # 
+   #    
    #    bin.size=1e6
+   #    genome.bins=rmd_bins.hg38
+   #    genome.bins=rmd_bins.hg19
    #    as.matrix=T
    #    vcf.filter='PASS'
-   #    clonal.variants.only=T
+   #    clonal.variants.only=F
+   #    mut.types='snv'
    #    verbose=T
    # }
    
+   ## Init --------------------------------
    if(!is.null(vcf.file)){
       df <- mutSigExtractor::variantsFromVcf(vcf.file, vcf.fields=c(1,2,4,5,7,8), vcf.filter=vcf.filter, verbose=verbose, ...)
+      #df <- mutSigExtractor::variantsFromVcf(vcf.file, vcf.fields=c(1,2,4,5,7,8), vcf.filter=vcf.filter, verbose=verbose)
    }
    
+   ## Subset for clonal variants
    if(clonal.variants.only & nrow(df)!=0){
       if(verbose){ message('Selecting clonal variants') }
       ## Split clonal/subclonal variants
@@ -56,7 +61,7 @@ extractRmd <- function(
    }
    df$info <- NULL
    
-   ## --------------------------------
+   ## Sanitize vcf input
    if(nrow(df)!=0){
       df_colnames <- c('chrom','pos','ref','alt')
       if(!(identical(colnames(df)[1:4], df_colnames))){
@@ -66,11 +71,12 @@ extractRmd <- function(
       
       if(verbose){ message('Removing rows with multiple ALT sequences...') }
       df <- df[!grepl(',',df$alt),]
-       
-      if(verbose){ message('Converting chrom name style...') }
+      
+      if(verbose){ message("Setting chrom name style for vcf to 'NCBI'...") }
       GenomeInfoDb::seqlevelsStyle(df$chrom)<- 'NCBI'
    }
    
+   ## Subset for mut types
    if(nrow(df)!=0){
       
       if(verbose){ message('Subsetting for: ',paste(mut.types, collapse=', '),'...') }
@@ -80,46 +86,80 @@ extractRmd <- function(
       df$mut_type[ nchar(df$ref)==2 & nchar(df$alt)==2 ] <- 'dbs'
       df$mut_type[ 
          ( nchar(df$ref)==1 & nchar(df$alt)>1 ) |
-         ( nchar(df$ref)>1 & nchar(df$alt)==1 )
+            ( nchar(df$ref)>1 & nchar(df$alt)==1 )
       ] <- 'indel'
       
       df <- df[df$mut_type %in% mut.types,]
    }
-
+   
    ## --------------------------------
    if(verbose){ message('Counting muts per bin...') }
-   if(is.null(genome.bins)){
-      genome.bins <- mkGenomeBins(bin.size=bin.size)
+   
+   ## Initialize counts
+   if(is.character(genome.bins)){
+      if(verbose){ message("Reading in genome bins @ ", genome.bins) }
+      genome.bins <- read.delim(genome.bins) 
    }
-   GenomeInfoDb::seqlevelsStyle(genome.bins$chrom )<- 'NCBI'
    
-   counts <- structure(rep(0, nrow(genome.bins)), names=genome.bins$bin_name )
+   counts <- structure(rep(0, nrow(genome.bins)), names=unique(genome.bins$bin_name) )
    
+   ## Main
    if(nrow(df)!=0){
-      df <- df[df$chrom %in% genome.bins$chrom,]
-      df$bin_name <- (function(){
-         #counter <- 0
-         l <- Map(function(chrom,pos){
-            #counter <<- counter + 1
-            #print(counter)
-            which(
-               chrom==genome.bins$chrom &
-               pos>=genome.bins$start &
-               pos<=genome.bins$end
-            )
-         }, df$chrom, df$pos)
-         
-         item_lengths <- sapply(l,length)
-         
-         if(any(item_lengths>1)){ 
-            stop("Some variants fall into multiple bins (overlapping bin coordinates)") 
-         }
-         
-         l[item_lengths==0] <- NA
-         genome.bins$bin_name[ unlist(l, use.names=F) ]
-      })()
       
-      tab <- table( df[!is.na(df$bin_name),'bin_name'] )
+      ##
+      if(verbose){ message("Setting chrom name style for `genome.bins` to 'NCBI'...") }
+      genome.bins$chrom <- as.character(genome.bins$chrom)
+      GenomeInfoDb::seqlevelsStyle(genome.bins$chrom)<- 'NCBI'
+      
+      ##
+      if(verbose){ message("Convert chrom:pos to integer representation...") }
+      df <- df[df$chrom %in% genome.bins$chrom,]
+      uniq_chroms <- naturalsort::naturalsort(unique(genome.bins$chrom))
+      genome.bins$chrom <- factor(genome.bins$chrom, uniq_chroms)
+      df$chrom <- factor(df$chrom, uniq_chroms)
+      
+      max_pos_digits <- nchar(max(c(genome.bins$start, genome.bins$end, df$pos)))
+      chromPosToInt <- function(chrom, pos, n.digits=max_pos_digits){
+         # chrom=genome.bins$chrom
+         # pos=genome.bins$start
+         # n.digits=max_pos_digits
+         
+         pos_char <- formatC(pos, width=n.digits, format='d', flag='0')
+         chrom_int <- as.integer(chrom)
+         as.numeric(paste0(chrom_int, pos_char))
+      }
+      
+      genome.bins$start_int <- chromPosToInt(genome.bins$chrom, genome.bins$start)
+      genome.bins$end_int <- chromPosToInt(genome.bins$chrom, genome.bins$end)
+      df$pos_int <- chromPosToInt(df$chrom, df$pos)
+      
+      ##
+      if(verbose){ message("Determining RMD bin for each mutation in the vcf...") }
+      matched_bins <- lapply(df$pos_int, function(i){
+         #i=df$pos_int[8221]
+         genome.bins$bin_name[ i>=genome.bins$start_int & i<=genome.bins$end_int ]
+      })
+      
+      match_lengths <- sapply(matched_bins, length)
+      n_warn_matches <- sum(match_lengths>1 | match_lengths==0)
+      
+      if(n_warn_matches>0){ 
+         warn_matches_summary <- table(match_lengths)
+         warn_matches_summary <- warn_matches_summary[names(warn_matches_summary)!=1]
+         warn_matches_summary <- paste0(warn_matches_summary, ' variants in ', names(warn_matches_summary),' bins')
+         warn_matches_summary <- paste(warn_matches_summary, collapse=', ')
+         
+         warning(
+            n_warn_matches,'/',nrow(df)," variants were matched to zero or multiple bins:\n",
+            warn_matches_summary,'.\n',
+            "For variants matched to >=2 bins, the first bin will be assigned"
+         ) 
+      }
+      
+      if(verbose){ message("Counting bin occurrences...") }
+      matched_bins[match_lengths==0] <- NA
+      df$bin_name <- sapply(matched_bins,`[[`,1)
+      tab <- table( df$bin_name[!is.na(df$bin_name)] )
       counts[names(tab)] <- tab
    }
    
@@ -128,7 +168,7 @@ extractRmd <- function(
    
    if(!as.matrix){ return(counts) }
    out <- as.matrix(counts)
-
+   
    colnames(out) <-
       if(is.null(sample.name)){
          if(!is.null(vcf.file)){ basename(vcf.file) }
@@ -136,6 +176,7 @@ extractRmd <- function(
       } else {
          sample.name
       }
-
+   
    return(out)
 }
+
